bec960b1cf20bf0c879fdb4cb0711f91d21e76e9
moraivanec <128829593+moraivanec@users.noreply.github.com>
2024-09-22 21:35:51 -0300

    Ejercicio_8

---
+++ Trabajo Practico N2/Ejercicio_8.py
@@ -0,0 +1,6 @@
+'''
+Utilizar la técnica de listas por comprensión para construir una lista con todos los
+números impares comprendidos entre 100 y 200.
+'''
+numeros_impares = [num for num in range(101, 200, 2)]
+print(numeros_impares)
